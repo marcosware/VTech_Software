873dc78559f78bcf24605b6d088ccfc7d7fce963
Wellington Junio <wjuniossantos009@gmail.com>
2025-04-28 19:41:03 -0300

    Gerenciamento de diretórios

---
+++ Project/src/main/java/VdeVigilancia/Projeto_OS/ProjetoOsApplication.java
@@ -0,0 +1,13 @@
+package VdeVigilancia.Projeto_OS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoOsApplication.class, args);
+	}
+
+}
