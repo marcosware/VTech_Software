460911dc6b9d4cccce48af813cf21649788c49aa
Wellington Junio <wjuniossantos009@gmail.com>
2025-04-28 19:24:07 -0300

    Primeiro comm it

---
+++ src/test/java/VdeVigilancia/Projeto_OS/ProjetoOsApplicationTests.java
@@ -0,0 +1,13 @@
+package VdeVigilancia.Projeto_OS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
