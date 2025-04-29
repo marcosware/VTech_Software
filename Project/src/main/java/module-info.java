c0a1bfd776b7736a5556add15e4973f4aaa5743a
Lucas Reis <luxtreme12365@gmail.com>
2025-04-29 19:11:12 -0300

    Module Info criado

---
+++ Project/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module VdeVigilancia.Projeto_OS {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    requires java.desktop;
+    requires com.dlsc.formsfx;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+
+    exports VdeVigilancia.Projeto_OS;
+    opens VdeVigilancia.Projeto_OS to javafx.fxml;
+}
